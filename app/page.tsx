ca6a6e455b12c3c128ba86a2d701f4457efb34b1
Jamie Piper <jamiescript@github.com>
2024-02-08 14:07:53 +0000

    component squares

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Square from "./components/Square.component";
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
+        <Square value="1" />
         <button className="square">2</button>
         <button className="square">3</button>
       </div>
