84defbcc330e6ec5dbb2f109558c724488fcdbe8
Jamie Piper <jamiescript@github.com>
2024-02-08 20:38:26 +0000

    lift state

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import Square from "./components/Square/Square.component";
+import Board from "./components/Board/Board.component";
 
-export default function Board() {
-  return (
-    <>
-      <div className="board-row">
-        {[1, 2, 3].map((value) => (
-          <Square key={value} value={value.toString()} />
-        ))}
-      </div>
-      <div className="board-row">
-        {[4, 5, 6].map((value) => (
-          <Square key={value} value={value.toString()} />
-        ))}
-      </div>
-      <div className="board-row">
-        {[7, 8, 9].map((value) => (
-          <Square key={value} value={value.toString()} />
-        ))}
-      </div>
-    </>
-  );
+export default function Page() {
+  return <Board />;
 }
